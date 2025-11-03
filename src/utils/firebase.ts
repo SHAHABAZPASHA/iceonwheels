387500507bcb5fc9ddb61256a11bf40a0f1cd3e0
shahabazpasha <shahabazpasha76@gmail.com>
2025-11-03 11:57:42 +0530

    Chore: update firebase.ts for correct Firestore config and env usage

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB05HWexjeD0nVKvLDfbCzzNzfYGzFhe1o",
-  authDomain: "iceonwheels-a635d.firebaseapp.com",
-  projectId: "iceonwheels-a635d",
-  storageBucket: "iceonwheels-a635d.firebasestorage.app",
-  messagingSenderId: "377895661693",
-  appId: "1:377895661693:web:92976a68d0ac495e480114"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
